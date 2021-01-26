ba1561fe5115960c906b256945142b4add8c096a
Paul Hudson <paul.hudson@gmail.com>
2021-01-26 13:17:23 +0000

    Removed the requirement for .sitrep.yml.

--- Sources/Sitrep/Command.swift
+++ Sources/Sitrep/Command.swift
@@ -11,26 +11,37 @@ import SitrepCore
 
 struct Command: ParsableCommand {
     @Option(name: .shortAndLong, help: "The report format \(Scan.ReportType.args).")
-    var format: Scan.ReportType = .json
+    var format: Scan.ReportType = .text
     
     @Option(name: [.short, .customLong("config")], help: "The path of `.sitrep.yml`.")
     var configurationPath: String?
     
     @Option(name: .shortAndLong, help: "The path of your project.")
     var path = FileManager.default.currentDirectoryPath
+
+    @Flag(name: .shortAndLong, help: "Print configuration settings then exit.")
+    var showConfiguration = false
     
     func run() throws {
         let configurationPath = self.configurationPath ?? self.path + "/.sitrep.yml"
-        let configuration = try Configuration.parse(configurationPath)
-        print("""
-            format: \(self.format.rawValue)
-            path: \(path)
-            configuration path: \(configurationPath)
-            configuration: \(configuration)
-            """)
-        
-        let url = URL(fileURLWithPath: path)
-        let app = Scan(rootURL: url)
-        app.run(reportType: self.format, configuration: configuration)
+        let configuration: Configuration
+
+        if FileManager.default.fileExists(atPath: configurationPath) {
+            configuration = try Configuration.parse(configurationPath)
+        } else {
+            configuration = Configuration.default
+        }
+
+        if showConfiguration {
+            print("""
+                Using configuration: \(configurationPath)
+                Configuration: \(configuration)
+                Scan path: \(path)
+                """)
+        } else {
+            let url = URL(fileURLWithPath: path)
+            let app = Scan(rootURL: url)
+            app.run(reportType: self.format, configuration: configuration)
+        }
     }
 }
